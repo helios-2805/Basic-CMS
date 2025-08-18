a40a8344263f470650cf5b4a8bc7105d1e9c85e4
Surya Sai <saisuryapalthi@gmail.com>
2025-08-18 20:29:04 +0530

    fixed the username/email check condition

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -81,7 +81,7 @@ const registerUser = asyncHandler( async (req, res) => {
 
 const loginUser = asyncHandler(async (req, res) => {
   const { email, username, password } = req.body
-  if(!username || !email) {
+  if(!(username || email)) {
     throw new apiError(404, "Username or Email is required!!");
   }
 
